86e11ba8f966e839bf68bb9a8cbee250261a4ba5
xwz0108 <2253423181@qq.com>
2026-05-27 17:38:36 +0800

    Fix TS errors: src/components/game/GameProvider.tsx

--- src/components/game/GameProvider.tsx
+++ src/components/game/GameProvider.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
-import { useGameStore } from '../../store/gameStore';
+// import useGameStore from '../../store/gameStore';
 import GameBoard from './GameBoard';
 
 const GameProvider: React.FC = () => {
